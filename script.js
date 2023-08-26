3fe4f4efcb0c12340eccc70dfaca37eeca05f805
Gina Cahya Utami <ginacu12@gmail.com>
2023-08-26 13:06:58 -0700

    add active navbar to current pages

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-$('#navbar .nav-link').on('click', function() {
-    $('#navbar .nav-link').removeClass('active');
-    $('#navbar .nav-link').addClass('active');
-});
-
-
-// Making active nav link indicator
-// give us the current location of the page like index.html etc
-// const activePage = window.location.pathname;
-
-// // thinks function is says "we have this nav link and we gonna lop through 'em n then we gonna check the link href in it"
-// const navLinks = document.querySelectorAll('nav ul a').forEach(link => {
-//     // if the link href is include the same name in activePage then we add the class 'active'
-//     if(link.href.includes(`${activePage}`)){
-//         link.classList.add('active');
-//     } else {
-//         ('#navbar .nav-link').classList.remove('active');
-//         ('#about').classList.add('active')
-//     }
-// });
-
 // Work with the json for projects data, this is using jquery
 
 function showAllProjects() {
